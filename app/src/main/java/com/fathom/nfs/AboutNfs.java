14fe83cabee924e124fbfabf04645f28bca1c047
abdallahFathoom <abdallah@fthm.me>
2020-03-25 17:17:47 +0300

    "About Layout done"

--- app/src/main/java/com/fathom/nfs/AboutNfs.java
+++ app/src/main/java/com/fathom/nfs/AboutNfs.java
@@ -1,18 +1,21 @@
 package com.fathom.nfs;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.ViewCompat;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-
 public class AboutNfs extends Fragment {
+
+    private ConstraintLayout aboutContent;
 
 
 
@@ -29,5 +32,14 @@ public class AboutNfs extends Fragment {
         return inflater.inflate(R.layout.fragment_about_nfs, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        aboutContent = view.findViewById(R.id.aboutNfsContent);
+
+
+        ViewCompat.setLayoutDirection(aboutContent, ViewCompat.LAYOUT_DIRECTION_LTR);
+
+    }
 }
